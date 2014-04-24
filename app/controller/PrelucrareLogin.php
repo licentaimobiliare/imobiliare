5e0709b573b6c8e3af394bc928e4b9cce6b47b3e
laurentiustroia <stroia.laurentiu92@gmail.com>
2014-04-24 19:21:22 +0300

    prelucreaza explicatiile

--- app/controller/PrelucrareLogin.php
+++ app/controller/PrelucrareLogin.php
@@ -25,18 +25,16 @@ class controller_PrelucrareLogin {
 	$_SESSION['txtpass']=$_POST['txtpass'];
        // var_dump($_SESSION);
         //echo "da";die;
-	 $sql = $connection->prepare("SELECT nume_user, parola FROM users");
-         $sql ->execute();
+	 $sql = $connection->prepare("SELECT nume_user, parola FROM users where nume_user=?");
+         $sql ->execute(array($_POST['txtuser'],$_POST['txtpass']));
          $resursa = $sql -> fetchAll(PDO::FETCH_OBJ);
                          
-                       
+        if(count($resursa) == 0) nu exista useru
+            else if($resursa[0]->parola == $_POST['txtpass']) ok
+                else a gresit parola
                        //var_dump($resursa)  ;
 	
-	while($row=mysql_fetch_array($resursa))
-	{
-		if($_SESSION['txtuser']==$row['nume_user'] && $_SESSION['txtpass']==$row['parola']) $ok=1;//verificam daca exista memorata o persoana cu username-ul 
-																		//si parola specifica
-	}
+
 	if ($ok==1)//in caz afirmativ afisare username-ul clientului
 	{
 		
