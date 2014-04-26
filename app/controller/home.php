4cc083087f1643c9e21cd9e65b2b982fdf40a41b
laurentiustroia <stroia.laurentiu92@gmail.com>
2014-04-26 14:59:51 +0300

    remove debug

--- app/controller/home.php
+++ app/controller/home.php
@@ -8,7 +8,6 @@ class controller_home {
 	 * This is the homepage.
 	 */
 	function action_index($params) {
-        echo '<pre>';print_r(model_imobil::getById(1));die;
         
 		@include_once APP_PATH . 'view/home_index.tpl.php';
 	
