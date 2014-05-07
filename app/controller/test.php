ce54cf1251d32299362b6d811b1ecb171d223ae6
biancaioaniciu <bianca_ioaniciu@yahoo.com>
2014-05-07 03:37:17 +0300

    completare model date imobil

--- app/controller/test.php
+++ app/controller/test.php
@@ -9,11 +9,13 @@ class controller_test {
 	 * This is the homepage.
 	 */
 	function action_index($params) {
-           $exemplu=array('finisaj'=>'semifinisat');
+           $exemplu=array('nume_user'=>'maria','parola'=>'123','mentiuni'=>'12sfg','telefon'=>'125496','email'=>'asd@yahoo.com','tip'=>'user','iduser'=>15 );
             echo "<pre>";
-            print_r(model_DateImobil :: FinisajGetByName($exemplu));die;
+//            print_r(model_DateImobil :: deleteproprietari(123));die;
 //                        print_r(model_imobil :: getById(array(1,2)));die;
-            
+//            print_r(model_DateImobil :: tiGetByName('hala'));die;
+            print_r(model_DateImobil :: updateusers($exemplu));die;
+//            print_r(model_DateImobil :: proprietariListName('2'));die;
 //            print_r(model_DateImobil :: deletefinisaj(100));die;
 		$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
 		echo json_encode($arr);
--- app/model/DateImobil.php
+++ app/model/DateImobil.php
@@ -46,69 +46,47 @@ class model_DateImobil{
    
    
    public static function FinisajGetByName($name)
-   {
-       
+   {       
        $connection= model_database::get_instance();
-       $stmt = $connection -> prepare("Select * from finisaje where finisaj=?" );
-       
+       $stmt = $connection -> prepare("Select * from finisaje where finisaj=?" );       
        $stmt-> execute(array($name));
-       var_dump($stmt);
-       //var_dump($results);
        $results = $stmt ->fetchObject(); 
-       var_dump($results);
        return $results;
        
    }
    
     public static function FinisajListName($name)
    {
-       
        $connection= model_database::get_instance();
-       $stmt = $connection -> prepare("Select * from finisaje where finisaj ='?'" );
-       
-       $stmt-> execute(array($name).'%');
-       //var_dump($stmt);
-       //var_dump($results);
-       $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
-       //var_dump($results);
+       $stmt = $connection -> prepare("Select * from finisaje where finisaj like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
        return $results;
-       
    }
    
    public static function deletefinisaj($id)
    {   
          $connection= model_database::get_instance();
         
-         $stmt =$connection->prepare("delete from finisaje where idf= $id");    
-         //var_dump($stmt);
+         $stmt =$connection->prepare("delete from finisaje where idf= $id");           
          try{
-            $stmt->execute(array($id));
-           // var_dump($values);
+            $stmt->execute(array($id));         
             return true;
         } 
-        catch(Exception $e){
-            
+        catch(Exception $e){  
             return false;
-            
         }  
    }
    
-   public static function updatefinisaj($id)
-   {
-       $connection= model_database::get_instance();
-        
-         $stmt =$connection->prepare("update from finisaje where idf= $id");    
-         //var_dump($stmt);
-         try{
-            $stmt->execute(array($id));
-           // var_dump($values);
-            return true;
-        } 
-        catch(Exception $e){
-            
-            return false;
-            
-        }  
+   public static function updatefinisaj($a)
+   {         
+         $connection= model_database::get_instance();
+         $stmt =$connection->prepare("update finisaje set finisaj=? where idf=?");    
+         $stmt->execute(array($a['finisaj'],$a['idf']));
+         $object = new stdClass();
+         $object->finisaj = $a['finisaj'] ;
+         $object->idf = $a['idf'] ;  
+         return $object;
    }
    
     public static function adaugatc($tc){
@@ -553,6 +531,442 @@ class model_DateImobil{
         }
         return $results;
    }
+   
+    public static function deletetc($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from tip_constructii where idtc= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deletetl($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from tip_locuinte where idtl= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deleteti($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from tip_imobil where idti= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deletets($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from tip_strazi where idts= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deletenumar($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from numar where numar= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deletecp($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from coduri_postale where idcp= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deleteusers($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from users where iduser= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deletemarkers($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from markers where id= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deleteservicii($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from servicii where ids= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deleteclienti($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from clienti where cnp= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function deleteproprietari($id)
+   {   
+         $connection= model_database::get_instance();
+        
+         $stmt =$connection->prepare("delete from proprietari where cnp= $id");           
+         try{
+            $stmt->execute(array($id));         
+            return true;
+        } 
+        catch(Exception $e){  
+            return false;
+        }  
+   }
+   
+   public static function tcGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from tip_constructii where tip_constructie=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;     
+   }
+   
+    public static function tcListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from tip_constructii where tip_constructie like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function tlGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from tip_locuinte where tip_locuinta=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;    
+   }
+   
+    public static function tlListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from tip_locuinte where tip_locuinta like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function tiGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from tip_imobil where tip_imobil=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;      
+   }
+   
+    public static function tiListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from tip_imobil where tip_imobil like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function tsGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from tip_strazi where tip_strada=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;     
+   }
+   
+    public static function tsListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from tip_strazi where tip_strada like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function numarGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from numar where numar=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;     
+   }
+   
+    public static function numarListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from numar where numar like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function cpGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from coduri_postale where cod_postal=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;       
+   }
+   
+    public static function cpListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from coduri_postale where cod_postal like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function usersGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from users where nume_user=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;      
+   }
+   
+    public static function usersListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from users where nume_user like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function markersGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from markers where name=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;     
+   }
+   
+    public static function markersListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from markers where name like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function serviciiGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from servicii where serviciu=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;     
+   }
+   
+    public static function serviciiListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from servicii where serviciu like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function clientiGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from clienti where cnp=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;     
+   }
+   
+    public static function clientiListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from clienti where cnp like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function proprietariGetByName($name)
+   {       
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from proprietari where cnp=?" );       
+       $stmt-> execute(array($name));
+       $results = $stmt ->fetchObject(); 
+       return $results;
+   }
+   
+    public static function proprietariListName($name)
+   {
+       $connection= model_database::get_instance();
+       $stmt = $connection -> prepare("Select * from proprietari where cnp like ?" );     
+       $stmt-> execute(array($name.'%'));
+        $results = $stmt -> fetchAll(PDO::FETCH_OBJ);
+       return $results;
+   }
+   
+   public static function updatetc($a)
+   {         
+         $connection= model_database::get_instance();
+         $stmt =$connection->prepare("update tip_constructii set tip_constructie=? where idtc=?");    
+         $stmt->execute(array($a['tip_constructie'],$a['idtc']));
+         $object = new stdClass();
+         $object->tip_constructie = $a['tip_constructie'] ;
+         $object->idtc = $a['idtc'] ;  
+         return $object;
+   }
+   
+   public static function updatetl($a)
+   {         
+         $connection= model_database::get_instance();
+         $stmt =$connection->prepare("update tip_locuinte set tip_locuinta=? where idtl=?");    
+         $stmt->execute(array($a['tip_locuinta'],$a['idtl']));
+         $object = new stdClass();
+         $object->tip_locuinta = $a['tip_locuinta'] ;
+         $object->idtl = $a['idtl'] ;  
+         return $object;
+   }
+   
+    public static function updateti($a)
+   {         
+         $connection= model_database::get_instance();
+         $stmt =$connection->prepare("update tip_imobil set tip_imobil=? where idti=?");    
+         $stmt->execute(array($a['tip_imobil'],$a['idti']));
+         $object = new stdClass();
+         $object->tip_imobil = $a['tip_imobil'] ;
+         $object->idti = $a['idti'] ;  
+         return $object;
+   }
+   
+    public static function updatets($a)
+   {         
+         $connection= model_database::get_instance();
+         $stmt =$connection->prepare("update tip_strazi set tip_strada=? where idts=?");    
+         $stmt->execute(array($a['tip_strada'],$a['idts']));
+         $object = new stdClass();
+         $object->tip_strada = $a['tip_strada'] ;
+         $object->idts = $a['idts'] ;  
+         return $object;
+   }
+   
+    public static function updatenumar($a)
+   {         
+         $connection= model_database::get_instance();
+         $stmt =$connection->prepare("update numar set numar=? where idn=?");    
+         $stmt->execute(array($a['numar'],$a['idn']));
+         $object = new stdClass();
+         $object->numar = $a['numar'] ;
+         $object->idn = $a['idn'] ;  
+         return $object;
+   }
+   
+    public static function updatecp($a)
+   {         
+         $connection= model_database::get_instance();
+         $stmt =$connection->prepare("update coduri_postale set cod_postal=? where idcp=?");    
+         $stmt->execute(array($a['cod_postal'],$a['idcp']));
+         $object = new stdClass();
+         $object->cod_postal = $a['cod_postal'] ;
+         $object->idcp = $a['idcp'] ;  
+         return $object;
+   }
+   
+    public static function updateusers($a)
+   {         
+         $connection= model_database::get_instance();
+         $stmt =$connection->prepare("update users set nume_user=?,parola=?,mentiuni=?,telefon=?,email=?,tip=? where iduser=?");    
+         $stmt->execute(array($a['nume_user'],$a['parola'],$a['mentiuni'],$a['telefon'],$a['email'],$a['tip'],$a['iduser']));
+         $object = new stdClass();
+         $object->nume_user = $a['nume_user'] ;
+         $object->parola = $a['parola'] ;
+         $object-> mentiuni= $a['mentiuni'] ;
+         $object->telefon = $a['telefon'] ;
+         $object->email = $a['email'] ;
+         $object->tip = $a['tip'] ;
+         $object->iduser = $a['iduser'] ;  
+         return $object;
+   }
+   
+   
 }
 
 
