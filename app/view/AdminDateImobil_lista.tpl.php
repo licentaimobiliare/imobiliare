75f67d97dd6bb4e7530b27d9a296739e48299571
Stroia Laurentiu <stroia.laurentiu92@gmail.com>
2014-06-09 22:02:30 +0300

    actualizare exemplu

--- app/view/AdminDateImobil_lista.tpl.php
+++ app/view/AdminDateImobil_lista.tpl.php
@@ -3,9 +3,9 @@
         ?>
 <p data-id="<?php echo $data->view_id;?>"><?php echo $data->view_name; ?></p>
 <?php    } ?>
-<input type="hidden" data-type="<?php echo $params[0]; ?>" />
+
 <div>
-    <a>Actualizare</a>
+    <a href="/AdminDateImobil/adaugare/<?php echo $params[0];?>/">Actualizare</a>
     <a>Sterge</a>
 </div>
 
