d2ef7f126d57ce011b981e2f901a64b77d264b4c
Stroia Laurentiu <stroia.laurentiu92@gmail.com>
2014-06-22 16:08:34 +0300

    fix bug with add markers

--- app/model/DateImobil.php
+++ app/model/DateImobil.php
@@ -244,14 +244,14 @@ class model_DateImobil{
     
     public static function adaugamarkers($marker){
     $keys=array();$values=array();
-        foreach($markers as $key => $value){
+        foreach($marker as $key => $value){
                 $keys[]=$key;
                 $values[]=$value;
             }
      
         $connection= model_database::get_instance();
         
-         $stmt =$connection->prepare('insert into markers('.implode(',', $keys).') values ('. str_pad('', count($values) * 2 - 1, '?,') . ')');        
+         $stmt =$connection->prepare('insert into markers('.implode(',', $keys).') values ('. str_pad('', count($values) * 2 - 1, '?,') . ')');
          try{
             $stmt->execute($values);
             return $connection->lastInsertId();
